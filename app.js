43b7789a72096cb6d41c8189268b445731da60cf
riffatrehman123 <riffatabdulrehman78@gmail.com>
2021-04-18 06:01:09 +0500

    project complete

---
+++ app.js
@@ -0,0 +1,386 @@
+                                                //  chapter # 1 
+
+
+
+
+//1. Write a script to greet your website visitor using JS alert box.
+// alert("Hello World!");
+
+//2. Write a script to display following message on your web page:
+//  alert("Error! please entre a the valid password.");
+
+//3. Write a script to display following message on your web
+//page: (Hint : Use line break)
+//    alert("Welcome to JS Land!!!\nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+//    alert("Welcome to JS land!")
+
+// alert("happy coding \nPrevant this page creating additional dialogs.");
+
+// 5. Generate the following message through browser’s
+//     developer console:
+// alert("Hello...i can run  JS through  my web browser's console");
+
+/// 6.. Make use of alerts in your new/existing HTML & CSS project.
+
+                                        
+
+
+                                                    //chapter # 2
+                                        
+                                        
+
+
+
+
+//1. Declare a variable called username.
+//  var username;
+
+
+// 2. Declare a variable called myName & assign to it a strin that represents your Full Name.
+//3. Write script to
+//a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+// var message = "Hello world";
+//  alert(message);
+
+
+//4. Write a script to save student’s bio data in JS variables andshow the data in alert boxes.
+
+//var Name= "jhon deo";
+//var age = "15 years old";
+//var certified = "Certified Mobile Application Development"
+//alert(Name);
+//alert(age);
+//alert(certified);
+
+
+//5. Write a script to display the following alert using one JS variable:
+
+//var Name= "pizza,\n pizz, \npiz, \npi, \np";
+//alert(Name);
+
+//6. Declare a variable called email and assign to it a string that represents your Email Address(e.g.example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: usestring concatenation)
+
+//var email ="riffatabdulrehman@gmail.com";
+//alert( email);
+
+//7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following
+//message in an alert box:
+
+
+
+//var x =" Im trying to learn from the Book A smarter way to learn javaScript";
+//alert(x);
+
+
+//8. Write a script to display this in browser through JS
+
+//document.write('yah! I can write HTML content in Jawascript');
+
+
+//9. Store following string in a variable and show in alert and browser through JS
+
+//var x = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(x);
+
+
+
+
+                                                   // chapter # 3
+
+
+
+ //1. Declare a variable called age & assign to it  your age. Show your age in an alert box.
+
+// var age ="I am 15 years old";
+ //alert(age);
+                                                                                                   
+ //2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her
+ //number of visits on your web page. For example: “Youhave visited this site N times”.v
+
+
+ //var z="you have visited this site 14 times";
+ //alert(z);
+
+
+// 3. Declare a variable called birthYear & assign to it yourbirth year. Show the following message in your browser:
+
+
+ // document.writeln("My birth year is 1990 <br>Data type of my declared variable is number");
+
+
+ //4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+//a. Visitor’s name
+//b. Product title
+//c. Quantity i.e.
+ //how many products a visitor wants to order
+//Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+//document.write(" John doe  ordered <b> 5 T-shirt(s) on XYZ clothing Store");
+
+                                                // --End--
+
+
+
+
+
+                                                         // chapter # 4
+    //1. Declare 3 variables in one statement 
+    
+   // var person = riffat,rehman,111
+
+
+    //2. Declare 5 legal & 5 illegal variable names.
+
+//legal variable;
+//var myName;
+//var rehman;
+//var $165;
+//var _riffat;
+//var rehman50;
+
+//illegal variable;
+//var 16riffat
+//var @Document
+//var riffat%
+//var *AudioListener
+//var &history
+
+
+//3. Display this in your browser
+//a) A heading stating “Rules for naming JS variables”
+//b) Variable names can only contain ___$___, ______,
+//______ and ______.
+//For example $my_1stVariable
+//c) Variables must begin with a ______, ______ or
+//_____. For example $name, _name or name
+//d) Variable names are case _________
+//e) Variable names should not be JS _________
+
+ //var heading="<h5>Rules for naming JS variables<h5>";
+//document.write(heading);
+//var heading= "Variable names can only contain $, underscorealphabet and numbers.For example $my_1stVariableVariables <br> must begin with a riffat, abdul or rehman.<br> For example $name, _name or name Variable names are case sensitive <br>Variable names should not be JS keywords"
+//document.write(heading)
+
+
+                                                   // --END--
+
+
+
+                                                         // chapter # 5
+
+    // 1. Write a program that take two numbers & add them in a
+       // new variable. Show the result in your browser.
+
+//var a= 3;
+//var b= 5;
+//var c= a+b;
+//document.write(c);
+
+
+//2. Repeat task1 for subtraction, multiplication, division &modulus.
+//subtraction
+
+//var a = 3;
+//var b = 5;
+//var c = a-b;
+//document.write(c);
+
+
+//multiplication
+//var a = 3;
+//var b = 5;
+//var c = a*b;
+//document.write(c);
+
+//division
+//var a = 3;
+//var b = 5;
+//var c = a/b;
+//document.write(c);
+
+//modulus
+//var a = 3;
+//var b = 5;
+//var c = a%b;
+//document.write(c);
+
+
+//3. Do the following using JS Mathematic Expressions
+//a. Declare a variable.
+//b. Show the value of variable in your browser like “Valueafter variable declaration is: ??”.
+//c. Initialize the variable with some number.
+//d. Show the value of variable in your browser like “Initialvalue: 5”.
+//e. Increment the variable.
+//f. Show the value of variable in your browser like “Valueafter increment is: 6”.
+//g. Add 7 to the variable.
+//h. Show the value of variable in your browser like “Valueafter addition is: 13”.
+//i. Decrement the variable.
+//j. Show the value of variable in your browser like “Valueafter decrement is: 12”.
+//k. Show the remainder after dividing the variable’s value
+//by 3.
+//l. Output : “The remainder is : 0”.
+
+//ans
+//var a = 5;
+//var b = ++a;
+//alert ( a );
+
+//var a = 6;
+//var b = 7;
+//alert (a+b);
+
+//var a = 13;
+//var b = --a;
+//alert (a);
+
+//var a = 12;
+//var b = 3;
+//alert (a%b);
+
+
+
+//4. Cost of one movie ticket is 600 PKR. Write a script tostoreticket price in a variable & calculate the cost of buying 5tickets
+//to a movie. Example output:
+
+//var a = 600;
+//var b = 5;
+//alert (a*b);
+
+//5. Write a script to display multiplication table of anynumber in your browser. E.g
+
+//document.write("Table of <b>6</b><br><br>")
+
+//for(var i = 1; i <= 10; i++)
+//{
+    //document.write("6 * "+ i +" = "+ 6*i+"<br>");
+//}
+
+//6. The Temperature Converter: It’s hot out! Let’s make aconverter based on the steps here.
+//a. Store a Celsius temperature into a variable.
+//b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+//c. Now store a Fahrenheit temperature into a variable.
+//d. Convert it to Celsius & output “NNoF is NNoC”.
+
+//var F=prompt("Entre temperiture in Fehrenheit");
+//var c =(c -32) * 5 / 9;
+//var F = (F *  9 / 5 ) + 32;
+//alert("temperiture in celsius ="+c);
+//document.write("temperiture in celsius -"+c);
+   
+
+
+//7. Write a program to implement checkout process of shopping cart system for an e-commerce website. Storethe following in variables
+
+//a. Price of item 1
+//b. Price of item 2
+//c. Ordered quantity of item 1
+//d. Ordered Quantity of item 2
+//e. Shipping charges
+//Compute the total cost & show the receipt in your browser.
+
+//var a = 850;
+//var b = 3;
+//var c = 200;
+
+//document.write(a * b + c );
+
+
+//8. Store total marks & marks obtained by a student in 2variables. Compute the percentage & show the result inyour browser
+
+                        // Mark-sheet
+//var obtained = 804; 
+//var total = 980; 
+//var percent = obtained*100/total; 
+//document.write(percent)
+
+//9. Assume we have 10 US dollars & 25 Saudi Riyals. Write ascript to convert the total currency to Pakistani Rupees.
+//Perform all calculations in a single expression.
+//(Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+//and 1 Saudi Riyal = 28 Pakistani Rupee)
+
+
+
+//var a = 10;
+//var b = 104;
+ //document.write(a * b);
+//var x = 25;
+//var y = 28;
+//document.write(x * y);
+//var c = 1040;
+//var z = 700;
+//document.write(c + z)
+
+
+
+
+//10. Write a program to initialize a variable with somenumber and do arithmetic in following sequence:
+//a. Add 5
+//b. Multiply by 10
+//c. Divide the result by 2
+//Perform all calculations in a single expression
+
+//var x = (5 *10) /2
+//document.write(x)
+
+
+//11. The Age Calculator: Forgot how old someone is?Calculate it!
+//a. Store the current year in a variable.
+//b. Store their birth year in a variable.
+//c. Calculate their 2 possible ages based on the storedvalues.
+//Output them to the screen like so: “They are either NN or NN
+//years old”.
+
+
+//var birthYear = 1990;
+//var futureYear  = 2027;
+//var age  = futureYear - birthYear;
+// document.write('I will be either ' + age + ' or ' + (age - 1));
+
+
+
+
+//12. The Geometrizer: Calculate properties of a circle.
+//a. Store a radius into a variable.
+//b. Calculate the circumference based on the radius, andoutput “The circumference is NN”.
+//(Hint : Circumference of a circle = 2 π r , π = 3.142)Calculate the area based on the radius, and output “The
+//area is NN”. (Hint : Area of a circle = π r2, π = 3.142)
+
+
+//var a = 20;
+//var b = 125.67;
+//document.write(a / b);
+
+
+
+//13. The Lifetime Supply Calculator: Ever wonder howmuch a “lifetime supply” of your favorite snack is?Wonder no more.
+//a. Store your favorite snack into a variable
+//b. Store your current age into a variable.
+//c. Store a maximum age into a variable.
+//d. Store an estimated amount per day (as a number).
+//e. Calculate how many would you eat total for the rest ofyour life.
+//Output the result to the screen like so: “You will need
+//NNNN to last you until the ripe old age of NN
+
+
+
+//var age = 28;
+//var maxAge = 100;
+//var numPerDay = 2;
+//var totalNeeded = (numPerDay * 365) * (maxAge - age);
+//var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+//console.log(message);
+
+
+
+
+                                                                            //chapte 1.
+ //1. Write a program to take a number in a variable, do the required arithmetic to display the following result in yourbrowser:
+
+ var a= 10;
+ var b = --a - a-- - a - a--
+ console.log(b);
